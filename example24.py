e6dd03747d685bacbc3f26dd61143b3e9834edd0
Dhana77777 <peddintidhanalaxmi7@gmail.com>
2025-08-27 14:26:40 +0530

    Add files via upload

---
+++ example24.py
@@ -0,0 +1,13 @@
+n=int(input("enter the number:"))
+fact=1
+for i in range(1,n+1):
+    fact=fact*i
+print(f"Factorial of {n}:{fact}")
+j=1
+fact1=1
+while(j<=5):
+    fact1=fact1*j
+    j=j+1
+
+print(f"Factorial of {n}:{fact1}")
+
