2f1a69ba265492e9ea3d07608e543b25f72e374b
Dhana77777 <peddintidhanalaxmi7@gmail.com>
2025-08-27 14:25:40 +0530

    Add files via upload

---
+++ example21.py
@@ -0,0 +1,13 @@
+n=int(input("enter the number:"))
+sum=0
+for i in range(1,n+1):
+    sum=sum+i
+print(f"Sum of first {n} natural numbers is:{sum}")
+print("by using while loop")
+j=1
+add1=0
+while(j<=n):
+    add1=add1+j
+    i=i+1
+print(f"the sum of first {n} natural numbers is :{add1}")
+
