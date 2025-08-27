737d52c3bea3fa424b4be6ce7b518d716b0f33ce
Dhana77777 <peddintidhanalaxmi7@gmail.com>
2025-08-27 14:27:00 +0530

    Add files via upload

---
+++ example25.py
@@ -0,0 +1,10 @@
+def sum1(a,b):
+    return a+b
+result=sum1(10,20)
+print (result)
+def sum2(a,b):
+    return a+b
+num1=int(input("enter the number1:"))
+num2=int(input("enter the number2:"))
+result=sum2(num1,num2)
+print("Summ of 2 numbers is:",result)
